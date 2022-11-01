37b2bd5ce0e505b86f26bab5529436e6011d4541
Lucas Cimon <925560+Lucas-C@users.noreply.github.com>
2022-11-01 18:05:18 +0100

    Minor fixups in build_gopher_dir.py

--- build_gopher_dir.py
+++ build_gopher_dir.py
@@ -12,14 +12,16 @@ from shutil import rmtree
 SRC_DIR = Path(__file__).resolve().parent
 DST_DIR = Path(sys.argv[1])
 
+def is_published(md_path):
+    content = md_path.open().read()
+    return not any(f"Status: {status}" in content for status in ("draft", "hidden"))
+
 rmtree(str(DST_DIR), ignore_errors=True)
 DST_DIR.mkdir()
 for md_article in SRC_DIR.glob("content/*.md"):
-    content = md_article.open().read()
-    if not any(f"Status: {status}" in content for status in ("draft", "hidden")):
+    if is_published(md_article):
         (DST_DIR / md_article.name).symlink_to(md_article)
 (DST_DIR / "pages").mkdir()
 for md_page in SRC_DIR.glob("content/pages/*.md"):
-    content = md_page.open().read()
-    if not any(f"Status: {status}" in content for status in ("draft", "hidden")):
+    if is_published(md_page):
         (DST_DIR / "pages" / md_page.name).symlink_to(md_page)
